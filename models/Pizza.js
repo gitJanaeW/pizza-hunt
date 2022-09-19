3090e5252c954569bbc2490646398094f86d907b
janae.wel <janae.wel@gmail.com>
2022-09-19 12:03:38 -0400

    added comments + association to Pizza

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,22 +1,40 @@
 const {Schema, model} = require('mongoose');
 
 // schema for the model
-const PizzaSchema = new Schema({
-    pizzaName: {
-        type: String
+const PizzaSchema = new Schema(
+    {
+        pizzaName: {
+            type: String
+        },
+        createdBy: {
+            type: String
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
+        size: {
+            type: String,
+            default: 'Large'
+        },
+        toppings: [],
+        comments: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Comment'
+            }
+        ]
     },
-    createdBy: {
-        type: String
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    },
-    size: {
-        type: String,
-        default: 'Large'
-    },
-    toppings: []
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false // Virtuals automatically return an id. To turn this off, set id to false
+    }
+);
+
+PizzaSchema.virtual('commentCount').get(function() {
+    return this.comments.length;
 });
 
 // declaring the creation of the model
